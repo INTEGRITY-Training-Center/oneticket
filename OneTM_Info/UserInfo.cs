8268a81b15de6311afd7b8e8b0567198eb51e37e
akt-1312 <55472602+akt-1312@users.noreply.github.com>
2019-10-19 10:26:38 +0630

    Add new Property BarNyar to UserInfo.cs

--- OneTM_Info/UserInfo.cs
+++ OneTM_Info/UserInfo.cs
@@ -12,6 +12,7 @@ namespace OneTM_Info
         public string UserName { get; set; }
         public string Password { get; set; }
         public int UserType { get; set; }
+        public string BarNyar { get; set; }
 
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
