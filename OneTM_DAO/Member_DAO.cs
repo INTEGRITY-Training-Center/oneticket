7e8d3999e08911fd28ab1504b4b5ba13624720f3
aungthetnaing1995 <56748603+aungthetnaing1995@users.noreply.github.com>
2019-12-08 13:53:42 +0630

    Member_Dao By ATN

--- OneTM_DAO/Member_DAO.cs
+++ OneTM_DAO/Member_DAO.cs
@@ -8,30 +8,62 @@ using OneTM_DAO.DBML;
 
 namespace OneTM_DAO
 {
-   public class Member_DAO
+    public class Member_DAO
     {
-       public List<Member_Info> SelectAllMember_Info()
-       {
-           List<Member_Info> Mlist = new List<Member_Info>();
-           Member_Info Minfo;
-           using (OTMDataContext db=new OTMDataContext())
-           {
-               var data = (from a in db.Members select a).ToList();
-               foreach (var obj in data)
-               {
-                   Minfo = new Member_Info();
-                   Minfo.MemberID = obj.MemberID;
-                   Minfo.TeamID = obj.TeamID;
-                   Minfo.MemberNumber = obj.MemberNumber;
-                   Minfo.CreatedDate = obj.CreatedDate;
-                   Minfo.UpdatedDate = obj.UpdatedDate;
+        public void MemberInsert(Member_Info minfo)
+        {
+            Member m;
+            using (OTMDataContext db = new OTMDataContext())
+            {
+                m = new Member();
+                Guid ID = Guid.NewGuid();
+                m.MemberID = ID.ToString();
+                m.TeamID = ID.ToString();
+                m.MemberNumber = minfo.MemberNumber;
+                m.CreatedDate = minfo.CreatedDate;
+                m.UpdatedDate = minfo.UpdatedDate;
+                db.Members.InsertOnSubmit(m);
+                db.SubmitChanges();
+            }
+        }
 
-                   Mlist.Add(Minfo);
+        public void DeleteByMemberID(string MemberID)
+        {
 
-               }
-           }
+            using (OTMDataContext db = new OTMDataContext())
+            {
+                var data = (from a in db.Members where a.MemberID == MemberID select a).FirstOrDefault();
+                db.Members.DeleteOnSubmit(data);
+                db.SubmitChanges();
+            }
+        }
+        public List<Member_Info> SelectAllMember_Info()
+        {
+            List<Member_Info> Mlist = new List<Member_Info>();
+            Member_Info Minfo;
+            using (OTMDataContext db = new OTMDataContext())
+            {
+                var data = (from a in db.Members select a).ToList();
+                foreach (var obj in data)
+                {
+                    Minfo = new Member_Info();
+                    Minfo.MemberID = obj.MemberID;
+                    Minfo.TeamID = obj.TeamID;
+                    Minfo.MemberNumber = obj.MemberNumber;
+                    Minfo.CreatedDate = obj.CreatedDate;
+                    Minfo.UpdatedDate = obj.UpdatedDate;
 
-           return Mlist;
-       }
-    } 
+                    Mlist.Add(Minfo);
+
+                }
+            }
+
+            return Mlist;
+        }
+    }
+
+
+
+
+
 }
