5e4994732e97717ef75d0e0c0b8a9ffbc923cced
aungthetnaing1995 <56748603+aungthetnaing1995@users.noreply.github.com>
2019-12-15 13:25:23 +0630

    MemberController.cs

---
+++ OneTM_Controller/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneTM_Info;
+using OneTM_DAO;
+
+namespace OneTM_Controller
+{
+    class MemberController
+    {
+        Member_Info minfo;
+        Member_DAO Mdao;
+        public void Insert(TeamInfo tinfo)
+        {
+            Mdao = new Member_DAO();
+            Mdao.MemberInsert(minfo);
+        }
+    }
+}
