0b6a27ca18b96f747e76d249d0e14d9164c76d56
aungthetnaing1995 <56748603+aungthetnaing1995@users.noreply.github.com>
2019-12-15 12:51:29 +0630

    Severity dao by atn

---
+++ OneTM_DAO/SeverityDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneTM_DAO.DBML;
+using OneTM_Info;
+
+
+namespace OneTM_DAO
+{
+    class SeverityDAO
+    {
+        public List<SeverityInfo> SelectAllSeverityInfo()
+        {
+            List<SeverityInfo> slist = new List<SeverityInfo>();
+            SeverityInfo sinfo;
+
+            using (OTMDataContext db = new OTMDataContext())
+            {
+                var source = (from a in db.Severities select a).ToList();
+                foreach (var obj in source)
+                {
+                    sinfo = new SeverityInfo();
+                    sinfo.SeverityID = obj.SeverityID;
+                    sinfo.SeverityLevel = obj.SeverityLevel;
+                    
+
+                }
+            }
+            return slist;
+        }
+
+        public SeverityInfo selectBySeverityID(int SeverityID)
+        {
+            using (OTMDataContext db = new OTMDataContext())
+            {
+                var ID = (from a in db.Severities where a.SeverityID == SeverityID select a).FirstOrDefault();
+                SeverityInfo sinfo = new SeverityInfo();
+                sinfo.SeverityID = ID.SeverityID;
+                sinfo.SeverityLevel = ID.SeverityLevel;
+                return sinfo;
+            }
+        }
+    }
+}
